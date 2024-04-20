5f1aac7a86e0ca5fb81d351fce90f328057f942c
EmiartyGit <mert12552@gmail.com>
2024-04-20 21:05:32 +0300

    asd

---
+++ Assets/ItemDestroy.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroy : MonoBehaviour
+{
+
+    //[SerializeField] private PlayerInventory inventory;
+
+    public bool canDestroy = false;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            canDestroy = true;
+        }
+
+        if(Input.GetKeyUp(KeyCode.E))
+        {
+            canDestroy = false;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(canDestroy == true)// && collision.tag == "Pickable" )
+        {
+          //  inventory.hasApple = true;
+            Destroy(collision.gameObject);
+        }
+        /*if (canDestroy == true && collision.tag == "Carrot")
+        {
+          //  inventory.hasCarrot = true;
+            Destroy(collision.gameObject);
+        }
+        if (canDestroy == true && collision.tag == "Brush")
+        {
+           // inventory.hasBrush = true;
+            Destroy(collision.gameObject);
+        }
+        if (canDestroy == true && collision.tag == "Whip")
+        {
+           // inventory.hasWhip = true;
+            Destroy(collision.gameObject);
+        }*/
+    }
+}
