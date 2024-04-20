5affcc7212c6502791f37bef092a1715f9dec5c5
EmiartyGit <mert12552@gmail.com>
2024-04-21 01:12:59 +0300

    Holding ve Inventory

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb2d;
+    public PlayerInventory playerInventory;
     Vector2 movement;
     public float moveSpeed = 5f;
     public Animator animator;
@@ -25,6 +26,15 @@ public class PlayerController : MonoBehaviour
             animator.SetFloat("Horizontal", movement.x);
             animator.SetFloat("Vertical", movement.y);
             animator.SetFloat("Speed", movement.sqrMagnitude);
+
+            if (playerInventory.selectedItem > 0)
+            {
+                animator.SetBool("Holding", true);
+            }
+            else
+            {
+                animator.SetBool("Holding", false);
+            }
         }
 
 
